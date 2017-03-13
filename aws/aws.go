9643bb7abc1a6b127c066fb2cb75a6548823300c
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-14 00:24:15 +0900

    Retrieve CSV instead of JSON

--- aws/aws.go
+++ aws/aws.go
@@ -8,24 +8,24 @@ import (
 	"github.com/pkg/errors"
 )
 
-// RetrievePriceList downloads price list JSON
-func RetrievePriceList(service string) (string, error) {
-	url := priceListURL(service)
+// RetrievePriceList downloads price list CSV
+func RetrievePriceListCSV(service string) (string, error) {
+	url := priceListCSVURL(service)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to retrieve price list JSON")
+		return "", errors.Wrap(err, "failed to retrieve price list CSV")
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", errors.Wrap(err, "failed to read price list JSON body")
+		return "", errors.Wrap(err, "failed to read price list CSV body")
 	}
 
 	return string(body), nil
 }
 
-func priceListURL(service string) string {
-	return fmt.Sprintf("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/%s/current/index.json", service)
+func priceListCSVURL(service string) string {
+	return fmt.Sprintf("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/%s/current/index.csv", service)
 }
--- aws/aws_test.go
+++ aws/aws_test.go
@@ -6,19 +6,36 @@ import (
 	"github.com/h2non/gock"
 )
 
-func TestRetrievePriceList(t *testing.T) {
+func TestRetrievePriceListCSV(t *testing.T) {
 	defer gock.Off()
 
-	gock.New("https://pricing.us-east-1.amazonaws.com").Get("/offers/v1.0/aws/AmazonEC2/current/index.json").Reply(200).BodyString("{}")
+	gock.New("https://pricing.us-east-1.amazonaws.com").
+		Get("/offers/v1.0/aws/AmazonEC2/current/index.csv").
+		Reply(200).
+		BodyString(`"FormatVersion","v1.0"
+"Disclaimer","This pricing list is for informational purposes only. All prices are subject to the additional terms included in the pricing pages on http://aws.amazon.com. All Free Tier prices are also subject to the terms included at https://aws.amazon.com/free/"
+"Publication Date","2017-03-02T18:32:21Z"
+"Version","20170302183221"
+"OfferCode","AmazonEC2"
+"SKU","OfferTermCode","RateCode","TermType","PriceDescription","EffectiveDate","StartingRange","EndingRange","Unit","PricePerUnit","Currency","LeaseContractLength","PurchaseOption","OfferingClass","Product Family","serviceCode","Location","Location Type","Instance Type","Current Generation","Instance Family","vCPU","Physical Processor","Clock Speed","Memory","Storage","Network Performance","Processor Architecture","Storage Media","Volume Type","Max Volume Size","Max IOPS/volume","Max IOPS Burst Performance","Max throughput/volume","Provisioned","Tenancy","EBS Optimized","Operating System","License Model","Group","Group Description","Transfer Type","From Location","From Location Type","To Location","To Location Type","usageType","operation","Comments","Dedicated EBS Throughput","Enhanced Networking Supported","GPU","Instance Capacity - 10xlarge","Instance Capacity - 2xlarge","Instance Capacity - 4xlarge","Instance Capacity - 8xlarge","Instance Capacity - large","Instance Capacity - medium","Instance Capacity - xlarge","Intel AVX Available","Intel AVX2 Available","Intel Turbo Available","Physical Cores","Pre Installed S/W","Processor Features","Sockets"
+"YQHNG5NBWUE3D67S","4NA7Y494T4","YQHNG5NBWUE3D67S.4NA7Y494T4.6YS6EN2CT7","Reserved","Red Hat Enterprise Linux (Amazon VPC), m4.xlarge instance-hours used this month","2016-11-30","0","Inf","Hrs","0.2230000000","USD","1yr","No Upfront","standard","Compute Instance","AmazonEC2","US East (Ohio)","AWS Region","m4.xlarge","Yes","General purpose","4","Intel Xeon E5-2676 v3 (Haswell)","2.4  GHz","16 GiB","EBS only","High","64-bit",,,,,,,,"Dedicated",,"RHEL","No License required",,,,,,,,"USE2-DedicatedUsage:m4.xlarge","RunInstances:0010",,"750 Mbps","Yes",,,,,,,,,,,,,"NA","Intel AVX; Intel AVX2; Intel Turbo",
+`)
 
 	service := "AmazonEC2"
 
-	got, err := RetrievePriceList(service)
+	got, err := RetrievePriceListCSV(service)
 	if err != nil {
 		t.Errorf("error should not be raised: %s", err)
 	}
 
-	expected := "{}"
+	expected := `"FormatVersion","v1.0"
+"Disclaimer","This pricing list is for informational purposes only. All prices are subject to the additional terms included in the pricing pages on http://aws.amazon.com. All Free Tier prices are also subject to the terms included at https://aws.amazon.com/free/"
+"Publication Date","2017-03-02T18:32:21Z"
+"Version","20170302183221"
+"OfferCode","AmazonEC2"
+"SKU","OfferTermCode","RateCode","TermType","PriceDescription","EffectiveDate","StartingRange","EndingRange","Unit","PricePerUnit","Currency","LeaseContractLength","PurchaseOption","OfferingClass","Product Family","serviceCode","Location","Location Type","Instance Type","Current Generation","Instance Family","vCPU","Physical Processor","Clock Speed","Memory","Storage","Network Performance","Processor Architecture","Storage Media","Volume Type","Max Volume Size","Max IOPS/volume","Max IOPS Burst Performance","Max throughput/volume","Provisioned","Tenancy","EBS Optimized","Operating System","License Model","Group","Group Description","Transfer Type","From Location","From Location Type","To Location","To Location Type","usageType","operation","Comments","Dedicated EBS Throughput","Enhanced Networking Supported","GPU","Instance Capacity - 10xlarge","Instance Capacity - 2xlarge","Instance Capacity - 4xlarge","Instance Capacity - 8xlarge","Instance Capacity - large","Instance Capacity - medium","Instance Capacity - xlarge","Intel AVX Available","Intel AVX2 Available","Intel Turbo Available","Physical Cores","Pre Installed S/W","Processor Features","Sockets"
+"YQHNG5NBWUE3D67S","4NA7Y494T4","YQHNG5NBWUE3D67S.4NA7Y494T4.6YS6EN2CT7","Reserved","Red Hat Enterprise Linux (Amazon VPC), m4.xlarge instance-hours used this month","2016-11-30","0","Inf","Hrs","0.2230000000","USD","1yr","No Upfront","standard","Compute Instance","AmazonEC2","US East (Ohio)","AWS Region","m4.xlarge","Yes","General purpose","4","Intel Xeon E5-2676 v3 (Haswell)","2.4  GHz","16 GiB","EBS only","High","64-bit",,,,,,,,"Dedicated",,"RHEL","No License required",,,,,,,,"USE2-DedicatedUsage:m4.xlarge","RunInstances:0010",,"750 Mbps","Yes",,,,,,,,,,,,,"NA","Intel AVX; Intel AVX2; Intel Turbo",
+`
 	if got != expected {
 		t.Errorf("body does not match. expected: %q, got: %q", expected, got)
 	}
