3c00cb36f8138c2d54b882a0890d8a3480458993
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-22 00:38:14 +0900

    Parse EC2 price list CSV

---
+++ aws/ec2.go
@@ -0,0 +1,42 @@
+package aws
+
+import (
+	"encoding/csv"
+	"io"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	metadataLineCount = 5
+)
+
+// ParseEC2PriceListCSV converts EC2 price list CSV to Go object
+func ParseEC2PriceListCSV(body string) ([]string, [][]string, error) {
+	lines := strings.Split(body, "\n")
+	bodyWithoutMetadata := strings.Join(lines[metadataLineCount:], "\n")
+	reader := csv.NewReader(strings.NewReader(bodyWithoutMetadata))
+
+	header, err := reader.Read()
+	if err != nil {
+		return []string{}, [][]string{}, errors.Wrap(err, "failed to parse header line")
+	}
+
+	records := [][]string{}
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return []string{}, [][]string{}, errors.Wrap(err, "failed to parse line")
+		}
+
+		records = append(records, record)
+	}
+
+	return header, records, nil
+}
