3d102d8274fc9400081c0dfd62b4e7fb9eccc58d
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-22 01:18:45 +0900

    Show progressbar while downloading CSV

--- aws/aws.go
+++ aws/aws.go
@@ -2,9 +2,13 @@ package aws
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/cheggaaa/pb"
 	"github.com/pkg/errors"
 )
 
@@ -14,7 +18,7 @@ const (
 )
 
 // RetrievePriceListCSV downloads price list CSV
-func RetrievePriceListCSV(service string) (string, error) {
+func RetrievePriceListCSV(service string, showProgress bool) (string, error) {
 	url := priceListCSVURL(service)
 
 	resp, err := http.Get(url)
@@ -23,7 +27,24 @@ func RetrievePriceListCSV(service string) (string, error) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	i, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get Content-Length")
+	}
+
+	var reader io.Reader
+
+	if showProgress {
+		bar := pb.New(i).SetUnits(pb.U_BYTES).SetRefreshRate(10 * time.Millisecond)
+		bar.ShowSpeed = true
+		bar.Start()
+
+		reader = bar.NewProxyReader(resp.Body)
+	} else {
+		reader = resp.Body
+	}
+
+	body, err := ioutil.ReadAll(reader)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to read price list CSV body")
 	}
