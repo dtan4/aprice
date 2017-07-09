3ed9f7a99336e972b3203a4b18b5abdfb6842ed6
Daisuke Fujita <dtanshi45@gmail.com>
2017-07-09 21:34:21 +0900

    Move logic to ec2_init.go

--- main.go
+++ main.go
@@ -1,64 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/dtan4/aprice/aws"
-	"github.com/dtan4/aprice/aws/ec2"
-	"github.com/dtan4/aprice/db"
-)
-
-const (
-	// TODO: use absolute path
-	dbFilename = "aprice.db"
-	table      = "aprice_price_list"
+	"github.com/dtan4/aprice/cmd"
 )
 
 func main() {
-	fmt.Println("===> retrieving price list...")
-
-	csv, err := aws.RetrievePriceListCSV(aws.EC2Service, true)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("===> parsing price list...")
-
-	header, records, err := ec2.ParseEC2PriceListCSV(csv)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", err)
-		os.Exit(1)
-	}
-
-	d, err := db.NewSQLite3Client(dbFilename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("===> opening table...")
-
-	exists, err := d.TableExists(table)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", err)
-		os.Exit(1)
-	}
-
-	if !exists {
-		if err := d.CreateTable(table, header); err != nil {
-			fmt.Fprintf(os.Stderr, "%+v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	fmt.Println("===> inserting records...")
-
-	if err := d.ImportPriceList(table, header, records); err != nil {
-		fmt.Fprintf(os.Stderr, "%+v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("complete!")
+	cmd.Execute()
 }
